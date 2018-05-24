fd8c0618910fed1c84ba64759e9e5a0e518f828a
Krista Katzenmeyer <krissyishere@gmail.com>
2018-05-23 23:07:00 -0700

    + 1 so seconds are second and not first

--- utils/dateToStringWithoutSeconds.js
+++ utils/dateToStringWithoutSeconds.js
@@ -2,8 +2,14 @@
 function dateToStringWithoutSeconds(/* int */ timestamp) /* string */ {
 	const date = new Date(timestamp);
 	const string = date.toLocaleString();
+	console.log(string);
 	const indexOfFirstColon = string.indexOf(':');
-	const indexOfSecondColon = string.indexOf(':', indexOfFirstColon);
+	const indexOfSecondColon = string.indexOf(':', indexOfFirstColon    +  1);
+	console.log(indexOfFirstColon);
+	console.log(indexOfSecondColon);
+	console.log(string.substring(0, indexOfSecondColon));
+	console.log(string.substring(indexOfSecondColon + 3));
+
 	return string.substring(0, indexOfSecondColon) +
 		string.substring(indexOfSecondColon + 3);
 }
